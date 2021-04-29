38caeec183a4448f0927d6ce129fb370f1e7dc33
Kavin <kavinraj.m@thoughtworks.com>
2021-04-29 22:12:26 +0530

    Fix failing test cases for map

--- src/map.js
+++ src/map.js
@@ -1,5 +1,22 @@
-const map = (list, param) => {
+const head = require('./head');
+const tail = require('./tail');
 
+const map = (list, functionCall) => {
+    return mapListElements(list, [], functionCall);
+}
+
+const mapListElements = (inputList, outputList, functionCall) => {
+    if(inputList.length > 0)
+    {
+        const firstElement = head(inputList);
+        const outputElement = functionCall(firstElement);
+
+        const length = outputList.length;
+        outputList[length] = outputElement;
+
+        return mapListElements(tail(inputList), outputList, functionCall);
+    }
+    return outputList;
 }
 
 module.exports = map;
