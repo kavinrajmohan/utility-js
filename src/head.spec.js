46f803c40785c6ca269c29d2c406e4de7061b90c
Kavin <kavinraj.m@thoughtworks.com>
2021-04-29 21:43:30 +0530

    Fix failing test for Head

--- src/head.spec.js
+++ src/head.spec.js
@@ -5,4 +5,8 @@ describe('Head', () => {
     it('head([1, 2, 3]) should be 1', () => {
         expect(head([1, 2, 3])).toEqual(1);
     })
+
+    it('head([])  should be undefined', () => {
+        expect(head([])).toEqual(undefined);
+    })
 })
