2c3193c8e5c66b63d3245863b06d84e231b0e43a
Kavin <kavinraj.m@thoughtworks.com>
2021-04-29 21:49:43 +0530

    Add and fix failing test case for Tail

--- src/tail.spec.js
+++ src/tail.spec.js
@@ -1,8 +1,12 @@
 const tail = require('./tail');
 
 describe('Tail', () => {
-    it('tail([1, 2, 3])', () => {
+    it('tail([1, 2, 3]) should be [2,3]', () => {
         expect(tail([1, 2, 3])).toEqual([2,3]);
+    })
+
+    it('tail([]) should be []', () => {
+        expect(tail([])).toEqual([]);
     })
 
 
