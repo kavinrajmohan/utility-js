bb84f1e2a81665015cb51b2b6e6bc01fdf129def
Kavin <kavinraj.m@thoughtworks.com>
2021-04-30 14:37:16 +0530

    Add failing test for reduce

---
+++ src/reduce.js
@@ -0,0 +1,5 @@
+const reduce = () => {
+
+}
+
+module.exports = reduce;
---
+++ src/reduce.spec.js
@@ -0,0 +1,23 @@
+const reduce = require('./reduce');
+
+describe('Reduce', () => {
+
+    it('should return undefined when empty list is passed', () => {
+        const fun = (x, y) => x + y;
+
+        expect(reduce([], fun)).toEqual(undefined);
+    })
+
+    it('', () => {
+        
+    })
+
+    it('', () => {
+        
+    })
+
+    it('', () => {
+        
+    })
+
+})
