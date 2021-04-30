b17819abfefad958f0a634bc68d5037eb38d768e
Kavin <kavinraj.m@thoughtworks.com>
2021-04-30 14:52:36 +0530

    Fix the tests for reduce

--- src/reduce.js
+++ src/reduce.js
@@ -1,5 +1,27 @@
-const reduce = () => {
+const head = require('./head');
+const tail = require('./tail');
 
+const reduceEveryElement = (inputList, fun, value) => {
+    
+    if(inputList.length > 0){
+        const firstElement = head(inputList);
+
+        if(value){
+            value = fun(value, firstElement);
+        }
+        else{
+            value = firstElement;
+        }
+        return reduceEveryElement(tail(inputList), fun, value);
+    }
+    return value;
+}
+
+
+
+
+const reduce = (list, fun, value) => {
+    return reduceEveryElement(list, fun, value);
 }
 
 module.exports = reduce;
--- src/reduce.spec.js
+++ src/reduce.spec.js
@@ -8,16 +8,22 @@ describe('Reduce', () => {
         expect(reduce([], fun)).toEqual(undefined);
     })
 
-    it('', () => {
-        
+    it('should return 10 when empty list is passed with value 10', () => {
+        const fun = (x, y) => x + y;
+
+        expect(reduce([], fun, 10)).toEqual(10);
     })
 
-    it('', () => {
-        
+    it('should return abc when list of characters is passed with fun', () => {
+        const fun = (x, y) => x + y;
+
+        expect(reduce(['a', 'b', 'c'], fun)).toEqual("abc");
     })
 
-    it('', () => {
-        
+    it('should return zabc when list of charcters is passed with fun and value z', () => {
+        const fun = (x, y) => x + y;
+
+        expect(reduce(['a', 'b', 'c'], fun, 'z')).toEqual("zabc");
     })
 
 })
