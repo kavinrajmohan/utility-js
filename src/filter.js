6d472e38cf7120c2b38d258202b532c4520944e9
Kavin <kavinraj.m@thoughtworks.com>
2021-04-30 09:37:16 +0530

    Add failing test case for filter

---
+++ src/filter.js
@@ -0,0 +1,5 @@
+const filter = () => {
+
+}
+
+module.exports = filter;
---
+++ src/filter.spec.js
@@ -0,0 +1,27 @@
+const filter = require('./filter');
+
+describe('Filter', () => {
+
+    it('should return list when empty list is passed', () => {
+        const fun = () => true;
+
+        expect(filter([], fun)).toEqual([]);
+    })
+
+    it('', () => {
+        
+    })
+
+    it('', () => {
+        
+    })
+
+    it('', () => {
+        
+    })
+
+    it('', () => {
+        
+    })
+
+})
