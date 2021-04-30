4ccfedb4354cee259844a5bc2522441bcf35c850
Kavin <kavinraj.m@thoughtworks.com>
2021-04-30 15:05:34 +0530

    Fix failing tests for max

--- src/max.js
+++ src/max.js
@@ -1,5 +1,26 @@
-const max = (list) => {
+const head = require('./head');
+const tail = require('./tail');
 
+const findMax = (inputList, value) => {
+    if(inputList.length > 0)
+    {
+        const firstElement = head(inputList);
+
+        if(value)
+        {
+            value = value > firstElement ? value : firstElement;
+        }
+        else{
+            value = firstElement;
+        }
+        return findMax(tail(inputList), value);
+    }
+    return value;
+}
+
+const max = (list) => {
+    const value = undefined;
+    return findMax(list, value);
 }
 
 module.exports = max;
