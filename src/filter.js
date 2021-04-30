69fbdba8ee33edc5d6d4fcd6415b5e7caf0c67ba
Kavin <kavinraj.m@thoughtworks.com>
2021-04-30 09:50:53 +0530

    Fix failing test cases for filter

--- src/filter.js
+++ src/filter.js
@@ -1,5 +1,25 @@
-const filter = () => {
+const head = require('./head');
+const tail = require('./tail');
 
+const filter = (list, fun) => {
+    return filterInputElement(list, [], fun);
+}
+
+const filterInputElement = (inputList, outputList, fun) => {
+    if(inputList.length > 0)
+    {
+        const firstElement = head(inputList);
+        const isTrueToBeOutput = fun(firstElement);
+
+        if(isTrueToBeOutput)
+        {
+            const length = outputList.length;
+            outputList[length] = firstElement;
+        }
+
+        return filterInputElement(tail(inputList), outputList, fun);
+    }
+    return outputList;
 }
 
 module.exports = filter;
--- src/filter.spec.js
+++ src/filter.spec.js
@@ -8,20 +8,28 @@ describe('Filter', () => {
         expect(filter([], fun)).toEqual([]);
     })
 
-    it('', () => {
-        
+    it('should return same list when function returns true', () => {
+        const fun = () => true;
+
+        expect(filter([1,2,3], fun)).toEqual([1,2,3]);
     })
 
-    it('', () => {
-        
+    it('should return empty list when function return false', () => {
+        const fun = () => false;
+
+        expect(filter([1,2,3], fun)).toEqual([]);
     })
 
-    it('', () => {
+    it('should return list with numbers greater than 1 when function uses greater than logic', () => {
+        const fun = (value) => value > 1;
         
+        expect(filter([1,2,3], fun)).toEqual([2,3]);
     })
 
-    it('', () => {
-        
+    it('should return uppercase characters in the list when function is called', () => {
+        const fun = (character) => character.toUpperCase() === character;
+
+        expect(filter(['a', 'B', 'c', 'D'], fun)).toEqual(['B','D']);
     })
 
 })
