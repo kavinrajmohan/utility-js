1b63e4d4b9b8c1fab796478c92e15653e58b51a8
Kavin <kavinraj.m@thoughtworks.com>
2021-04-30 15:07:24 +0530

    Add failing test for min

---
+++ src/min.js
@@ -0,0 +1,5 @@
+const min = (list) => {
+
+}
+
+module.exports = min;
---
+++ src/min.spec.js
@@ -0,0 +1,14 @@
+const min = require('./min');
+
+describe("Max", () => {
+
+    it('should return undefined when empty list is passed', () => {
+        expect(min([])).toEqual(undefined);
+    })
+
+    it('should return 1 when list is passed', () => {
+        expect(min([1,2,3,4])).toEqual(1);
+    })
+
+
+})
