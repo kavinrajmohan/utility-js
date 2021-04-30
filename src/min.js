793308724ff3ce56404553ad661b892f042f3958
Kavin <kavinraj.m@thoughtworks.com>
2021-04-30 15:08:48 +0530

    Fix failing test for min

--- src/min.js
+++ src/min.js
@@ -1,5 +1,26 @@
-const min = (list) => {
+const head = require('./head');
+const tail = require('./tail');
 
+const findMin = (inputList, value) => {
+    if(inputList.length > 0)
+    {
+        const firstElement = head(inputList);
+
+        if(value)
+        {
+            value = value < firstElement ? value : firstElement;
+        }
+        else{
+            value = firstElement;
+        }
+        return findMin(tail(inputList), value);
+    }
+    return value;
+}
+
+const min = (list) => {
+    const value = undefined;
+    return findMin(list, value);
 }
 
 module.exports = min;
